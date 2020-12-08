dee34e1de1017894a5e19ef575c07e342ae0dcc3
blackWhiteRabbit <xueweidiqiu@163.com>
2020-12-08 23:52:56 +0800

    将@Controller注解改为@RestController注解

--- src/main/java/indi/bwr/demo/blog/controller/ArticleController.java
+++ src/main/java/indi/bwr/demo/blog/controller/ArticleController.java
@@ -3,14 +3,16 @@ package indi.bwr.demo.blog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import indi.bwr.demo.blog.entity.Article;
 import indi.bwr.demo.blog.service.ArticleService;
 
-@Controller
+@RestController
 @RequestMapping("/Article")
+@CrossOrigin
 public class ArticleController {
 	@Autowired
 	private ArticleService articleService;
